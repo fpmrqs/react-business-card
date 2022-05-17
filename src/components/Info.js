5ba7d4d5f1425309aa709bd11f041314ba382f12
Francisco Pottinger-Marques <fpottinger.marques@gmail.com>
2022-05-17 08:54:49 +0100

    added buttons to the info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,9 +2,19 @@ const Info = () => {
   return (
     <header>
       <div className="image"></div>
-      
       <h1>Ramses The Cat</h1>
       <h3>Full-Stack Engineer</h3>
+      <p>ramses@thecat.com</p>
+      <div className="buttons">
+        <button className="email-btn">
+          <img src="./images/Mail.png" alt="Email Icon"/>
+          <span>Email</span>
+        </button>
+        <button className="linkedin-btn">
+          <img src="./images/linkedin.png" alt="Email Icon"/>
+          <span>LinkedIn</span>
+        </button>
+      </div>
     </header>
   )
 }
